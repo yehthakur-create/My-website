0a9278d6145fbadcc2c13d11042ac0c8e96d1377
yehthakur-create <yehthakur@gmail.com>
2026-09-03 16:38:44 +0530

    Add click event to toggle completed class on habits

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const habits = document.querySelectorAll(".habit");
+
+habits.forEach(habit => {
+  habit.addEventListener("click", () => {
+    habit.classList.toggle("completed");
+  });
+});
 console.log("My NEET Habit Tracker is working!");
